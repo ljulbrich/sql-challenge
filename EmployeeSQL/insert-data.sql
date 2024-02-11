67b93472c7e360101cff7ed31e4f529d32d79b7d
ljulbrich <lucasulbrich@protonmail.com>
2024-02-11 20:29:01 +1100

    Creating project

--- EmployeeSQL/insert-data.sql
+++ EmployeeSQL/insert-data.sql
@@ -1,4 +1,4 @@
-CREATE TABLE employees (
+CREATE TABLE [IF NOT EXISTS] employees (
     emp_no SERIAL PRIMARY KEY,
     emp_title_id VARCHAR (255),
     birth_date VARCHAR (255),
@@ -8,27 +8,27 @@ CREATE TABLE employees (
     hire_date DATE
 );
 
-CREATE TABLE salaries (
+CREATE TABLE [IF NOT EXISTS] salaries (
     emp_no SERIAL,
     salary SERIAL PRIMARY KEY
 );
 
-CREATE TABLE titles (
+CREATE TABLE [IF NOT EXISTS] titles (
     title_id VARCHAR (255) PRIMARY KEY,
     title VARCHAR (255)
 );
 
-CREATE TABLE departments (
+CREATE TABLE [IF NOT EXISTS] departments (
     dept_no VARCHAR (255) PRIMARY KEY,
     dept_name VARCHAR (255)
 );
 
-CREATE TABLE dept_emp (
+CREATE TABLE [IF NOT EXISTS] dept_emp (
     emp_no VARCHAR (255),
     dept_no VARCHAR (255)
 );
 
-CREATE TABLE dept_managemer (
+CREATE TABLE [IF NOT EXISTS] dept_managemer (
     dept_no VARCHAR (255)
     emp_no VARCHAR (255)
 );
