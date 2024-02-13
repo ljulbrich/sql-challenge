d76953befa1d51d1efa478b765dd031193344358
ljulbrich <lucasulbrich@protonmail.com>
2024-02-13 17:27:37 +1100

    Added ERD image

--- EmployeeSQL/insert-data.sql
+++ EmployeeSQL/insert-data.sql
@@ -1,10 +1,10 @@
 CREATE TABLE [IF NOT EXISTS] employees (
     emp_no SERIAL PRIMARY KEY,
-    emp_title_id VARCHAR (255),
-    birth_date VARCHAR (255),
-    first_name VARCHAR (255),
-    last_name VARCHAR (255),
-    sex VARCHAR (50),
+    emp_title_id VARCHAR(255),
+    birth_date VARCHAR(255),
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    sex VARCHAR(50),
     hire_date DATE
 );
 
@@ -14,21 +14,21 @@ CREATE TABLE [IF NOT EXISTS] salaries (
 );
 
 CREATE TABLE [IF NOT EXISTS] titles (
-    title_id VARCHAR (255) PRIMARY KEY,
-    title VARCHAR (255)
+    title_id VARCHAR(255) PRIMARY KEY,
+    title VARCHAR(255)
 );
 
 CREATE TABLE [IF NOT EXISTS] departments (
-    dept_no VARCHAR (255) PRIMARY KEY,
-    dept_name VARCHAR (255)
+    dept_no VARCHAR(255) PRIMARY KEY,
+    dept_name VARCHAR(255)
 );
 
 CREATE TABLE [IF NOT EXISTS] dept_emp (
-    emp_no VARCHAR (255),
-    dept_no VARCHAR (255)
+    emp_no SERIAL,
+    dept_no VARCHAR(255)
 );
 
 CREATE TABLE [IF NOT EXISTS] dept_managemer (
-    dept_no VARCHAR (255)
-    emp_no VARCHAR (255)
+    dept_no VARCHAR(255),
+    emp_no SERIAL
 );
